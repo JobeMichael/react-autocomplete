bd080b1ace94e571a09ec84477e0c41808d3354f
Jobe Michael <jobumichael@gmail.com>
2018-04-05 16:26:18 +0530

    Modified Service + search in login

--- src/js/service.js
+++ src/js/service.js
@@ -1,5 +1,5 @@
 export const SERIVCE_URL = 'https://api.github.com/search/users';
 
 export const getSearchResult = (query = '') => {
-    return fetch(`${SERIVCE_URL}?q=${query}`).then(data => data.json());
+    return fetch(`${SERIVCE_URL}?q=${query}+in:login`).then(data => data.json());
 }
